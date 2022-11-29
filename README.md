76cbca9b275700a21872177e3a8bc32313415ff9
Andrei Tarassov <tamogenia@gmail.com>
2022-11-29 12:01:44 +0300

    My version291122v1

---
+++ README.md
@@ -0,0 +1,12 @@
+# evolution-bootcamp-E2E-home-task
+
+
+## Create a set of scenarios for the following training example:
+https://www.newegg.com/
+
+## Tests
+- Run `yarn install` to install all dependencies.
+- Run `yarn test` to run the tests.
+- The console output will show you the result.
+
+### Good luck!
---
+++ features/step-definitions/Bootcamp_E2E.js
@@ -0,0 +1,51 @@
+const { Given, When, Then } = require('@wdio/cucumber-framework');
+
+Given("I have loaded the page",
+ async ()=>{ await browser.url('https://www.newegg.com'); 
+});
+
+Given("I have closed the promo banner",
+ async ()=>{
+    const banner= await $('//*[@aria-label="mask"]');
+    try 
+    {await banner.waitForDisplayed({ timeout: 10000 });
+    if(banner.isDisplayedInViewport()){browser.refresh();}} 
+    catch (err){}
+});
+
+When("I enter the word {string} in the search bar", async (word)=>{
+    const searchBar=$('input[type=search]');
+    await searchBar.click(); 
+    await searchBar.setValue(word);
+});
+
+When("I click the search", async ()=>{ 
+  const searchBarButton=$('div.header2021-search-button');  
+  await searchBarButton.click();
+});
+
+Then("An item appears", async()=>{ 
+    const item= await $('//*[@class="item-cell"]');
+    await item.waitForDisplayed({ timeout: 10000 });
+    expect(item).toBeDisplayed;  
+    //item.click();
+});
+
+When("I open Today's Best Deals tab", async ()=>{ 
+    const tab= await $('#trendingBanner_720202');
+    tab.click(); 
+});
+
+When("I click on the Internet shop logo", async ()=>{ 
+   const logo=await $('div.header2021-nav.header2021-cart'); 
+   logo.click();   
+});
+
+Then("The main page opened", async()=>{ 
+    const cart= await $('#cart-top');
+    await cart.waitForDisplayed({ timeout: 10000 });   
+    await expect(browser).toHaveUrl('https://secure.newegg.com/shop/cart');
+});
+
+
+
